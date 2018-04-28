d5a60c04c1eb2692e7d60e24223eeee4c4446068
Josh <jhint2424@gmail.com>
2018-04-27 18:07:53 -0700

    adding apiRoutes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,14 @@
+//linking data
+var friendsData = require("../data/friend.js");
+//exporting get function of data and POST for survey results
+module.exports = function(app)    {
+    //shows data to the page when visited
+app.get("/api/friends", function(req, res)  {
+    res.json(friendsData);
+});
+//pushes friends data to JS array (does this nees JSON?)
+app.post("api/friends", function(req, res)  {
+    friendsData.push(req.body);
+});
+
+};
