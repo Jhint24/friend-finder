93a4ea3c7dcf6dd95ebf4c71e52d1cfcfe19a396
Josh <jhint2424@gmail.com>
2018-04-27 18:35:05 -0700

    adding server function

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+//sets up express app
+var app = express();
+//initial port to 3000, or to Heroku choice
+var PORT = process.env.PORT || 3000;
+//data parsing by express
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
+//require route files 
+require("./app/routing/apiroutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//start server with listener
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+})
