8e6f8a56cc18a3cee0334e9a7d784e2880380f43
Josh <jhint2424@gmail.com>
2018-04-27 18:19:07 -0700

    adding apiRoutes

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//data to store friends as an array of objects
+var friendArray = [
+    {
+        name: "Josh",
+        photo: "https://www.linkedin.com/in/jhint/",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+
+        ]
+    }
+];
+
+module.exports = friendArray;
