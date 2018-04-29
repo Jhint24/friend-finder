2129677b3d8ec8b843f1b7483baa44d5d8710045
Josh <jhint2424@gmail.com>
2018-04-29 13:16:53 -0700

    adding script logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,6 +9,7 @@ app.get("/api/friends", function(req, res)  {
 //pushes friends data to JS array (does this nees JSON?)
 app.post("api/friends", function(req, res)  {
     friendsData.push(req.body);
+    res.json(true);
 });
 
 };
