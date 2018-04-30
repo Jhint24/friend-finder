d07944e7d41822f8fe5d8c53233d81be9a4d8063
Josh <jhint2424@gmail.com>
2018-04-30 15:13:38 -0700

    finished logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,11 +8,52 @@ app.get("/api/friends", function(req, res)  {
 });
 //pushes friends data to JS array (does this nees JSON?)
 app.post("/api/friends", function(req, res)  {
-    friendsData.push(req.body);
-    res.json(true);
+//Comparing user with their best friend match 
+
+//best match
+var matchData = {
+    name: "",
+    photo: "",
+    friendDiff: 55
+};
+// the results into variables
+var userData 	= req.body;
+var userName 	= userData.name;
+var userPhoto 	= userData.photo;
+var userScores 	= userData.scores;
+
+var totalDifference = 0;
+//loop data
+for  (var i=0; i< friendsData.length; i++) {
+
+    console.log(friendsData[i].name);
+    totalDifference = 0;
+
+//loop scores
+    for (var j = 0; j < friendsData[i].scores[j]; j++){
+
+//simple maths using the score loops
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendsData[i].scores[j]));
+
+        //finds "Best" match
+        if (totalDifference <= matchData.friendDiff){
+
+            // Reset the matchData to be the new friend. 
+            matchData.name = friendsData[i].name;
+            matchData.photo = friendsData[i].photo;
+            matchData.friendDiff = totalDifference;
+        }
+    }
+}
+
+// push new data to friendData
+friendsData.push(userData);
+
+// used by html
+res.json(matchData);
 });
 
-};
+}
 // function getMatchData() {
 
 //     var currentUser = newMatch.scores;
