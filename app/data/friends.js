c07f7e33261113057d4b2adc56dcc1764c2a43dd
Josh <jhint2424@gmail.com>
2018-05-01 22:47:43 -0700

    added more objects

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,24 @@
 var friendArray = [
     {
         name: "Josh",
-        photo: "https://www.linkedin.com/in/jhint/",
+        photo: "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-11/256/frog-face.png",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            5
+
+        ]
+    },
+    {
+        name: "Johnny",
+        photo: "https://www.littledayout.com/wp-content/uploads/articles/2017/07-July/20170715-WorldEmojiDay/DabEmoji.jpg",
         scores: [
             5,
             5,
@@ -12,6 +29,40 @@ var friendArray = [
             5,
             5,
             5,
+            5,
+            1
+
+        ]
+    },
+    {
+        name: "Bella",
+        photo: "https://cdn57.androidauthority.net/wp-content/uploads/2016/05/female-emoji-new-york-times-840x517.jpg",
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+
+        ]
+    },
+    {
+        name: "Bucky",
+        photo: "https://9to5mac.files.wordpress.com/2017/10/silly.png?w=663&h=663&crop=1&quality=82&strip=all",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            2,
+            3,
+            4,
             5,
             5
 
